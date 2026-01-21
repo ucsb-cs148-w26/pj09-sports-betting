fae3bebefbb2dd691118b920effd6b09b8c3d9e0
Kevin <kevinyang.poke@gmail.com>
2026-01-21 11:48:59 -0800

    test for vercel deploy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p className="text-4xl font-bold">Hello, World!</p>
+      <p className="text-4xl font-bold">please no deploy</p>
     </div>
   );
 }
