052321992eff5a4211b9140786619b788f7f1128
Kevin <kevinyang.poke@gmail.com>
2026-01-26 15:41:27 -0800

    update probabilites calculation for final status

--- backend/probabilities.py
+++ backend/probabilities.py
@@ -12,13 +12,23 @@ Structured output:
 from typing import Any
 import random
 
+# TODO: Implement actual win probability calculation based on given stats
+def calculate(home_score: int, away_score: int, home_record: str, away_record: str, status: str) -> tuple[float, float]:
+    if status == "Final":
+        home_win_prob = 0 if home_score < away_score else 100
+        away_win_prob = 100 - home_win_prob
+    else:
+        home_win_prob = int(100 * random.random())
+        away_win_prob = 100 - home_win_prob
+    return home_win_prob, away_win_prob
+
 def compute_win_probabilities(games: list[dict[str, Any]]) -> dict[str, dict[str, float]]:
     result = {}
     for game in games:
-        random_prob = int(100 * random.random())
-        game_id = game["game_id"]
+        game_id, home_score, away_score, home_record, away_record, status = game["game_id"], game["home_score"], game["away_score"], game["home_record"], game["away_record"], game["status"]
+        home_win_prob, away_win_prob = calculate(home_score, away_score, home_record, away_record, status)
         result[game_id] = {
-            "home_win_prob": random_prob,
-            "away_win_prob": 100 - random_prob
+            "home_win_prob": home_win_prob,
+            "away_win_prob": away_win_prob
         }
     return result
