afb17f52767f18aa80bb85531b5b8498c014722f
Junhyung Yoon <junhyung@ucsb.edu>
2026-01-26 22:04:56 -0800

    added basic schema for possible database first design/SQL and Postgres design

---
+++ backend/sports-betting-db.sql
@@ -0,0 +1,51 @@
+-- This is a table to store the user information (could add more attributes)
+CREATE TABLE user_info (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- Table for showing the players' overall stats and not just for that specific game
+CREATE TABLE player_overall_info (
+    player_id INT PRIMARY KEY,
+    player_name VARCHAR(50) NOT NULL,
+    team VARCHAR(50),
+    player_position VARCHAR(50),
+    player_age INT,
+    player_height VARCHAR(50),
+    ppg FLOAT,
+    rpg FLOAT,
+    apg FLOAT,
+    spg FLOAT,
+    bpg FLOAT,
+    fg_percentage FLOAT,
+    three_pt_percentage FLOAT,
+    ft_percentage FLOAT
+);
+-- Table to hold info for the current game
+CREATE TABLE current_game_info (
+    game_id INT PRIMARY KEY,
+    game_date DATE NOT NULL,
+    home_team VARCHAR(50) NOT NULL,
+    home_team_score INT NOT NULL,
+    away_team VARCHAR(50) NOT NULL,
+    away_team_score INT NOT NULL,
+    game_time_elapsed TIME NOT NULL,
+    game_stadium VARCHAR(50)
+);
+-- Table to hold info for the players that are currently playing in the game
+CREATE TABLE player_in_game_info (
+    player_in_game_id INT PRIMARY KEY,
+    player_id INT REFERENCES player_overall_info(player_id),
+    game_id INT REFERENCES current_game_info(game_id),
+    game_date DATE NOT NULL,
+    points_total INT,
+    rebounds INT,
+    assists INT,
+    steals INT,
+    blocks INT,
+    minutes_played INT,
+    fg_made INT,
+    fg_attempted INT
+);
